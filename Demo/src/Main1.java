141a3a859f4388ecee865cb87a58f86cd3fec810
sdac-19 <sdac-19@sdac-19-PC>
2023-04-04 20:59:56 +0530

    v-1.0

--- Demo/src/Main1.java
+++ Demo/src/Main1.java
@@ -1,6 +1,8 @@
 
 public class Main1 {
-public static void main(String[] args) {
-	System.out.println("v-1.0");
-}
+
+	public static void main(String[] args) {
+		System.out.println("v-1.0");
+	}
+
 }
